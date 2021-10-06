55f9df47a7abbd84412975136fbb77b79bc64797
Eobard26 <1527198893@qq.com>
2021-10-06 11:08:31 +0800

    update sysinfotest.testproc

--- user/sysinfotest.c
+++ user/sysinfotest.c
@@ -107,7 +107,7 @@ void testproc() {
   }
   if(pid == 0){
     sinfo(&info);
-    if(info.nproc != nproc+1) {
+    if(info.nproc != nproc-1) {
       printf("sysinfotest: FAIL nproc is %d instead of %d\n", info.nproc, nproc+1);
       exit(1);
     }
